c80a17c00c177c6dcd216885e8d2f691d4aacbb1
Rounak Sen <103501472+rony0000013@users.noreply.github.com>
2023-08-12 14:56:40 +0530

    added blogs api route

--- app.py
+++ app.py
@@ -9,6 +9,8 @@ load_dotenv()
 #  URIs
 MONGO_URI = str(os.getenv("MONGO_URI"))
 
+
+
 # Create a new client and connect to the server
 client = MongoClient(MONGO_URI, server_api=ServerApi('1'))
 
@@ -20,6 +22,11 @@ except Exception as e:
     print(e)
 
 
+db = client["hack_database"]
+blogs_collection = db["blogs"]
+loc_collection = db["location"]
+
+
 
 app = FastAPI()
 
@@ -27,3 +34,13 @@ app = FastAPI()
 @app.get("/")
 async def read_root():
     return {"message": "Hello, world!"}
+
+
+@app.get("/blogs")
+def get_blogs():
+    blogs = []
+    for blog in blogs_collection.find():
+        blog["_id"] = str(blog["_id"])
+        blogs.append(blog)
+    return {"blogs": blogs}
+
